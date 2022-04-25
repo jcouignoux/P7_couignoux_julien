2979056d1700ee484d93c9d99d18c5e5b40fa040
Julien Couignoux <j.couignoux@gmail.com>
2022-04-25 19:36:40 +0200

    first version

---
+++ bruteforce.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import csv
+from itertools import combinations
+
+##################################################
+#################### SETUP #######################
+##################################################
+file = "./portefeuille.csv"
+max = 500
+
+##################################################
+#################### FUNCTIONS ###################
+##################################################
+
+
+def import_csv(file):
+    p_dict = dict()
+    with open(file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            p_dict[row['Actions']] = [float(row['Cout']), float(row['Benefice']),
+                                      float(row['Cout']) * float(row['Benefice']) / 100]
+    return p_dict
+
+
+def get_comb(p_dict):
+    comb_list = list()
+    for i in range(1, len(p_dict) + 1):
+        a = list(combinations(p_dict, i))
+        for comb in a:
+            costs = list()
+            rois = list()
+            [costs.append(p_dict[i][0]) for i in comb]
+            total = sum(costs)
+            if total <= max:
+                [rois.append(p_dict[i][2]) for i in comb]
+                roi = sum(rois)
+                comb_total = (comb, total, roi)
+                comb_list.append(comb_total)
+    return comb_list
+
+
+##################################################
+#################### MAIN FUNCTION ###############
+##################################################
+
+
+def main():
+    p_dict = import_csv(file)
+    comb_list = get_comb(p_dict)
+    best_comb = sorted(comb_list, key=lambda x: (-x[2], -x[1]))[0]
+    print(best_comb)
+
+
+if __name__ == '__main__':
+    main()
