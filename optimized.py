47c643ceb42dc24788390133b6830e2384e0a3e8
Julien Couignoux <j.couignoux@gmail.com>
2022-05-04 12:30:45 +0200

    new optimized with backpack algorythm

--- optimized.py
+++ optimized.py
@@ -1,26 +1,26 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 import csv
+from itertools import combinations
 from argparse import ArgumentParser
 import time
 import tracemalloc
-# import numpy as np
 import matplotlib.pyplot as plt
-# from statsmodels.nonparametric.kernel_regression import KernelReg
 
 
 ##################################################
 #################### SETUP #######################
 ##################################################
-file = "./portefeuille.csv"
-# file = './dataset1_Python+P7.csv'
+# file = "./portefeuille.csv"
+file = './dataset1_Python+P7.csv'
 # file = './dataset2_Python+P7.csv'
-max = 500
+maxi = 500
 
 parser = ArgumentParser()
 parser.add_argument('-a', '--analyse', action='store_true',
                     help='an integer for the accumulator')
 args = parser.parse_args()
+
 
 ##################################################
 #################### FUNCTIONS ###################
@@ -28,82 +28,115 @@ args = parser.parse_args()
 
 
 def import_csv(file):
-    p_dict = dict()
+    p_list = list()
     with open(file, 'r') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for row in reader:
+            name = row['name']
             price = abs(float(row['price']))
             profit = abs(float(row['profit']))
             roi = round(abs(float(row['price']) *
                         float(row['profit']) / 100), 2)
             if price != 0 and profit != 0 and roi != 0:
-                p_dict[row['name']] = [price, profit, roi]
-    return p_dict
+                p = (name, price, roi)
+                p_list.append(p)
+                # p_list.append([row['name'], float(row['price']), float(
+                #     row['price']) * float(row['profit']) / 100])
+            # p_list.append((row['name'], int(row['price']), int(
+            #     row['price']) * int(row['profit']) / 100))
+
+    return p_list
 
 
-def get_comb(p_dict):
-    sorted_p_dict = sorted(p_dict.items(), key=lambda x: (-x[1][1]))
-    best_comb = list()
+# def get_best_roi(p_list, p):
+#     a = 0
+#     b = len(p_list) - 1
+#     print(p)
+#     while a <= b:
+#         m = (a + b) // 2
+#         print(p_list)
+#         print(p_list[m][3])
+#         print(p[3])
+#         if p_list[m][3] > p[3]:
+#             b = m - 1
+#             best = p_list[m]
+#         elif p_list[m][3] < p[3]:
+#             a = m + 1
+#             best = p_list[m]
+#         else:
+#             b = m - 1
+#             best = p_list[m]
+
+#     return best
+
+
+def get_comb(maxi, p_list):
+    # print(p_list)
+    matrice = [[0 for x in range(maxi + 1)] for x in range(len(p_list) + 1)]
+
+    for i in range(1, len(p_list) + 1):
+        for w in range(1, maxi + 1):
+            if p_list[i-1][1] <= w:
+                index = int(w-p_list[i-1][1])
+                matrice[i][w] = max(
+                    # p_list[i-1][2] + matrice[i-1][w-p_list[i-1][1]], matrice[i-1][w])
+                    p_list[i-1][2] + matrice[i-1][index], matrice[i-1][w])
+            else:
+                matrice[i][w] = matrice[i-1][w]
+
+    w = maxi
+    n = len(p_list)
     total = 0
-    roi = 0
-    for action in sorted_p_dict:
-        if total + action[1][0] <= max:
-            total += action[1][0]
-            total = round(total, 2)
-            roi += action[1][2]
-            roi = round(roi, 2)
-            best_comb.append(action[0])
+    p_sel = []
 
-    return (tuple(best_comb), total, roi)
+    while w >= 0 and n >= 0:
+        p = p_list[n - 1]
+        index = int(w-p[1])
+        if matrice[n][int(w)] == matrice[n-1][index] + p[2]:
+            p_sel.append(p)
+            total += p[1]
+            w -= p[1]
+        n -= 1
 
-
-# def smooth(y, box_pts):
-#     box = np.ones(box_pts)/box_pts
-#     y_smooth = np.convolve(y, box, mode='same')
-#     return y_smooth
+    return total, matrice[-1][-1], p_sel
 
 
 def display_graph(list, y_label, title_label):
     print(list)
     x = range(1, len(list) + 1)
     y = list
-    # kr = KernelReg(y, x, 'c')
     plt.plot(x, y)
-    # plt.plot(x, smooth(y, 3))
-    # plt.plot(x, smooth(y, 19))
     plt.xlabel('x - Nb of Actions')
     plt.ylabel('y - ' + y_label)
     plt.title('optimized ' + title_label + '\n' +
               file.split('_')[0].split('/')[-1])
-    # plt.ylim(sorted(list)[0], sorted(list)[-1])
-    # plt.xlim(0, len(list))
     plt.show()
 
-
-##################################################
-#################### MAIN FUNCTION ###############
-##################################################
+    ##################################################
+    #################### MAIN FUNCTION ###############
+    ##################################################
 
 
 def main():
-    p_dict = import_csv(file)
+    p_list = import_csv(file)
     if args.analyse:
         time_list = list()
         mem_list = list()
-        for i in range(1, len(p_dict) + 1):
-            p_dict_list = list(p_dict.keys())[0:i]
-            p_dict_test = dict()
-            for action in p_dict_list:
-                p_dict_test[action] = p_dict[action]
+        for i in range(1, len(p_list) + 1, 50):
+            p_dict_list = p_list[0:i]
+            # p_dict_test = dict()
+            # for action in p_dict_list:
+            #     p_dict_test[action] = p_list[action]
             start = time.time()
-            best_comb = get_comb(p_dict_test)
+            best_comb = get_comb(maxi, p_dict_list)
+            # best_comb = sorted(comb_list, key=lambda x: (-x[2], -x[1]))[0]
             # print(best_comb)
             end = time.time()
             elapsed = end - start
-            # print(f'Temps d\'exécution : {elapsed:.6} s')
+            # print(f'Temps d\'exécution : {elapsed:.2} s')
             time_list.append(elapsed)
             tracemalloc.start()
-            best_comb = get_comb(p_dict)
+            comb_list = get_comb(maxi, p_dict_list)
             # print(tracemalloc.get_traced_memory())
             traced_mem = tracemalloc.get_traced_memory()
             used_mem = traced_mem[1] - traced_mem[0]
@@ -113,13 +146,12 @@ def main():
         display_graph(mem_list, 'Used Memory', 'Memory Analyse')
     else:
         start = time.time()
-        best_comb = get_comb(p_dict)
-        print(best_comb)
+        comb_list = get_comb(maxi, p_list)
+        # best_comb = sorted(comb_list, key=lambda x: (-x[2], -x[1]))[0]
+        print(comb_list)
         end = time.time()
         elapsed = end - start
         print(f'Temps d\'exécution : {elapsed:.2} s')
-        # best_comb = sorted(comb_list, key=lambda x: (-x[2], -x[1]))[0]
-        # print(best_comb)
 
 
 if __name__ == '__main__':
